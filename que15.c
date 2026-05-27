2749e5caede5128ed7fe9b04ffa5cbf0e4f04028
Rutuja-developer <rutujamk2004@gmail.com>
2026-05-28 00:42:41 +0530

    Update que15.c

--- que15.c
+++ que15.c
@@ -1,6 +1,4 @@
 //Accept on character from user and check whether that character is vowel(a,e,i,o,u) or not.
-//Input : E             Output : true
-//Input : d             Output : false
 
 #include<stdio.h>
 
